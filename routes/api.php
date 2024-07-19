a70c7f0ad761265c6d46ca32a07ec93d65f4d8f7
Isaac Oduh <ufuomaoduh@gmail.com>
2024-07-19 16:45:56 +0100

    implement categories endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController as APIAuthController;
+use App\Http\Controllers\API\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,6 +12,9 @@ Route::get('/user', function (Request $request) {
 Route::middleware(['auth:sanctum', 'admin'])->group(function () {
     Route::get('/user', [APIAuthController::class, 'getUser']);
     Route::post('/logout', [APIAuthController::class, 'logout']);
+
+    Route::apiResource('categories', CategoryController::class)->except('show');
+    Route::get('/categories/tree', [CategoryController::class, 'getAsTree']);
 });
 
 
