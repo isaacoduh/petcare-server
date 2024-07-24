3d2c384e94c2ecad2097ab5d077ab1eba5c2e9fa
Isaac Oduh <ufuomaoduh@gmail.com>
2024-07-24 18:20:13 +0100

    cart controller endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -16,11 +17,21 @@ Route::get('/', [ProductController::class, 'index'])->name('home');
 Route::get('/category/{category:slug}', [ProductController::class, 'byCategory'])->name('byCategory');
 Route::get('/product/{product:slug}', [ProductController::class, 'view'])->name('product.view');
 
+Route::prefix('/cart')->name('cart.')->group(function () {
+    Route::get('/', [CartController::class, 'index'])->name('index');
+    Route::post('/add/{product:slug}', [CartController::class, 'add'])->name('add');
+    Route::post('/remove/{product:slug}', [CartController::class, 'remove'])->name('remove');
+    Route::post('/update-quantity/{product:slug}', [CartController::class, 'updateQuantity'])->name('update-quantity');
+
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'view'])->name('profile');
     // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // cart -> index, add , product:slug, remove, update-quantity
 });
 
 require __DIR__.'/auth.php';
