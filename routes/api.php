59f11ab44df071f8d91b5df379e1465d6b7ecc1f
Isaac Oduh <ufuomaoduh@gmail.com>
2024-07-24 15:45:08 +0100

    implement endpoints for dashboard management api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\API\AuthController as APIAuthController;
 use App\Http\Controllers\API\CategoryController;
 use App\Http\Controllers\API\CustomerController;
+use App\Http\Controllers\API\DashboardController;
 use App\Http\Controllers\API\OrderController;
 use App\Http\Controllers\API\ProductController;
 use App\Http\Controllers\API\UserController;
@@ -27,7 +28,16 @@ Route::middleware(['auth:sanctum', 'admin'])->group(function () {
     Route::get('orders/statuses', [OrderController::class, 'getStatuses']);
     Route::post('orders/change-status/{order}/{status}', [OrderController::class, 'changeStatus']);
     Route::get('orders/{order}', [OrderController::class, 'view']);
-    
+
+
+    Route::get('/dashboard/customers-count', [DashboardController::class, 'activeCustomer']);
+    Route::get('/dashboard/products-count', [DashboardController::class, 'activeProducts']);
+    Route::get('/dashboard/orders-count', [DashboardController::class, 'paidOrders']);
+    Route::get('/dashboard/income-amount', [DashboardController::class, 'totalIncome']);
+    Route::get('/dashboard/orders-by-country', [DashboardController::class, 'ordersByCountry']);
+    Route::get('/dashboard/latest-customers', [DashboardController::class, 'latestCustomers']);
+    Route::get('/dashboard/latest-orders', [DashboardController::class, 'latestOrders']);
+
 
 });
 
