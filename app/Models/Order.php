dce7eabf071ffe44c4441eb3ec72b846f6023c78
Isaac Oduh <ufuomaoduh@gmail.com>
2024-07-24 14:20:18 +0100

    api feature for order management

---
+++ app/Models/Order.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\OrderStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['status', 'total_price', 'created_by', 'updated_by'];
+
+    public function isPaid()
+    {
+        return $this->status === OrderStatus::Paid->value;
+    }
+
+    public function payment(){}
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public static function deleteUnpaidOrders($hours)
+    {
+        return Order::query()->where('status', OrderStatus::Unpaid->value)
+            ->where('created_at', '<', Carbon::now()->subHours($hours))
+            ->delete();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\API\AuthController as APIAuthController;
 use App\Http\Controllers\API\CategoryController;
 use App\Http\Controllers\API\CustomerController;
+use App\Http\Controllers\API\OrderController;
 use App\Http\Controllers\API\ProductController;
 use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
@@ -21,6 +22,12 @@ Route::middleware(['auth:sanctum', 'admin'])->group(function () {
     Route::apiResource('users', UserController::class);
     Route::apiResource('customers', CustomerController::class);
     Route::get('/categories/tree', [CategoryController::class, 'getAsTree']);
+    Route::get('/countries', [CustomerController::class, 'countries']);
+    Route::get('orders', [OrderController::class, 'index']);
+    Route::get('orders/statuses', [OrderController::class, 'getStatuses']);
+    Route::post('orders/change-status/{order}/{status}', [OrderController::class, 'changeStatus']);
+    Route::get('orders/{order}', [OrderController::class, 'view']);
+    
 
 });
 
